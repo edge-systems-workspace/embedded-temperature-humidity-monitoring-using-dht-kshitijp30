49654674894be35259bf15a7430d83217cd0c476
Kshitij Singh <kshitij.verma_cs.aiml24@gla.ac.in>
2026-02-19 09:11:07 +0530

    update code formatting options and modify main file header

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,35 @@
  * and displays temperature and humidity values on Serial Monitor.
  * Students must complete the TODO sections.
  */
+
 #include <Arduino.h>
+#include  <DHT.h>
+#define DHTPIN 2
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE  );
+
+void setup(){
+    Serial.begin(9600);
+    dht.begin();
+    Serial.println("DHT11 Sensor Initialized");
+}
+
+void loop() {
+    float humidity = dht.readHumidity();
+    float temperature = dht.readTemperature();
+    if (isnan(humidity) || isnan(temperature)) {
+        Serial.println("Failed to read from DHT sensor!");
+        delay(2000);
+        return;
+    }
+    Serial.print("Humidity: ");
+    Serial.print(humidity);
+    Serial.print(" %  | ");
+    Serial.print("Temperature: ");
+    Serial.print(temperature);
+    Serial.println(" *");
+    delay(1500);
+}
 
 
